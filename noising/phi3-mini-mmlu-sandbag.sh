a90a5141a8d6d43d78c612246adf7fb66ca6aa3c
Your Name <you@example.com>
2026-07-05 14:26:38 +0200

    d

--- noising/phi3-mini-mmlu-sandbag.sh
+++ noising/phi3-mini-mmlu-sandbag.sh
@@ -1,1 +1,1 @@
-./.venv/bin/inspect  eval ./evals/mmlu-sandbag.py  --model hf/microsoft/Phi-3-mini-128k-instruct --max-connections 8
+./.venv/bin/inspect  eval ./evals/mmlu-sandbag.py  --model hf/microsoft/Phi-3-mini-128k-instruct --max-connections 8   -M noise_std=0.004 -M seed=0
