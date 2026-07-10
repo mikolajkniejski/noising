1fde21f805026b858ff96cb651cbb2472b11f0ae
Your Name <you@example.com>
2026-07-10 11:28:55 +0200

    acp

--- noising/phi3-mini-mmlu-sandbag.sh
+++ noising/phi3-mini-mmlu-sandbag.sh
@@ -1,4 +1,5 @@
 CUBLAS_WORKSPACE_CONFIG=:4096:8
 noise=${1:-0}
-printf "running with noise %s\n" $noise
-./.venv/bin/inspect  eval ./evals/mmlu-sandbag.py  --model noised/microsoft/Phi-3-mini-128k-instruct --max-connections 6   -M noise_std=$noise -M seed=0 -M do_sample=false
+tags=${2:-'placeholder'}
+printf "running with noise %s and tags %s\n" $noise $tags
+./.venv/bin/inspect  eval ./evals/mmlu-sandbag.py  --model noised/microsoft/Phi-3-mini-128k-instruct --max-connections 2 -M noise_std=$noise -M seed=0 -M do_sample=false --tags $tags
---
+++ noising/r.sh
@@ -0,0 +1,7 @@
+./phi3-mini-mmlu-sandbag.sh 0 run1
+./phi3-mini-mmlu-sandbag.sh 0.01 run1
+./phi3-mini-mmlu-sandbag.sh 0.03 run1
+./phi3-mini-mmlu-sandbag.sh 0.05 run1
+./phi3-mini-mmlu-sandbag.sh 0.04 run1
+./phi3-mini-mmlu-sandbag.sh 0.07 run1
+./phi3-mini-mmlu-sandbag.sh 0.09 run1
