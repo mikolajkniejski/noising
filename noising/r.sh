1bcb3981a97bd3934f56ba45768e95f0fae88848
Your Name <you@example.com>
2026-07-10 12:08:15 +0200

    acp

--- noising/r.sh
+++ noising/r.sh
@@ -1,7 +1,7 @@
-./phi3-mini-mmlu-sandbag.sh 0 run1
-./phi3-mini-mmlu-sandbag.sh 0.001 run1
-./phi3-mini-mmlu-sandbag.sh 0.003 run1
-./phi3-mini-mmlu-sandbag.sh 0.005 run1
-./phi3-mini-mmlu-sandbag.sh 0.004 run1
-./phi3-mini-mmlu-sandbag.sh 0.007 run1
-./phi3-mini-mmlu-sandbag.sh 0.009 run1
+./phi3-mini-mmlu-sandbag.sh 0 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.001 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.003 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.005 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.004 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.007 run2-new
+./phi3-mini-mmlu-sandbag.sh 0.009 run2-new
