069063a8afb4fc4aef9f111ab26293b08e0b1f3e
Your Name <you@example.com>
2026-07-10 21:32:40 +0200

    acp

--- noising/analyze.py
+++ noising/analyze.py
@@ -1,21 +1,21 @@
 from inspect_ai import log
 
-l = log.list_eval_logs("./logs")
+l = log.list_eval_logs("./logs", filter=lambda x: "no_sandbag" in x.tags and x.status == "success")
 
 ac = []
 no = []
 seds = []
 for i in l:
     llll  = log.read_eval_log(i, header_only=True)
-    if "run14" in llll.tags and llll.status == "success":
-        noise = llll.eval.model_args.get('noise_std', 100)
-        seed = llll.eval.model_args.get('seed', 100)
-        seds.append(seed)
-        # breakpoint()
-        if llll.results:
-            _ac = llll.results.scores[0].metrics['accuracy'].value
-            ac.append(_ac)
-        no.append(noise)
+    breakpoint()
+    noise = llll.eval.model_args.get('noise_std', 100)
+    seed = llll.eval.model_args.get('seed', 100)
+    seds.append(seed)
+    # breakpoint()
+    if llll.results:
+        _ac = llll.results.scores[0].metrics['accuracy'].value
+        ac.append(_ac)
+    no.append(noise)
 
 
 
