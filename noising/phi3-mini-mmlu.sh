e5be63abe436703c6b700b2184ca834c0e2fba43
Your Name <you@example.com>
2026-07-10 10:45:31 +0200

    acp

--- noising/phi3-mini-mmlu.sh
+++ noising/phi3-mini-mmlu.sh
@@ -1,1 +1,1 @@
-./.venv/bin/inspect  eval ./evals/mmlu.py  --model hf/microsoft/Phi-3-mini-128k-instruct --max-connections 8
+./.venv/bin/inspect  eval ./evals/mmlu.py  --model hf/microsoft/Phi-3-mini-128k-instruct --max-connections 8 --tags TEST
